230f7bf71fb763f19252d1e78fb74d8cd966a201
Anthonios Partheniou <partheniou@google.com>
2021-08-03 20:55:21 +0000

    Update GA_TEST_PROPERTY_ID

--- samples/noxfile_config.py
+++ samples/noxfile_config.py
@@ -13,7 +13,7 @@ TEST_CONFIG_OVERRIDE = {
     # A dictionary you want to inject into your test. Don't put any
     # secrets here. These values will override predefined values.
     "envs": {
-        "GA_TEST_PROPERTY_ID": "222596558",
+        "GA_TEST_PROPERTY_ID": "276206997",
         "GA_TEST_ACCOUNT_ID": "123",
         "GA_TEST_USER_LINK_ID": "123",
         "GA_TEST_ANDROID_APP_DATA_STREAM_ID": "123",
